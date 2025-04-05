96076f46836b4f59ce9fae6b1488aa3d5e3150b1
marwanmoub <moubayedmarwancoding@gmail.com>
2025-04-05 14:05:13 -0700

    Integrate Prettier and eslint and customize tabs

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import prettier from 'eslint-plugin-prettier';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    plugins: {
+      js,
+      prettier,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      'prettier/prettier': 'error',
+    },
+  },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+]);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
-  presets: [require("nativewind/preset")],
+  content: ['./app/**/*.{js,jsx,ts,tsx}'],
+  presets: [require('nativewind/preset')],
   theme: {
     extend: {
       colors: {
         primary: '#030014',
         accent: '#AB8BFF',
-        secondary: "#151312",
+        secondary: '#151312',
         light: {
-          100: "#D6C6FF",
-          200: "A8B5DB",
-          300: "9CA4AB"
+          100: '#D6C6FF',
+          200: 'A8B5DB',
+          300: '9CA4AB',
         },
         dark: {
           100: '#221F3D',
-          200: '#0f0d23'
-        }
-      }
+          200: '#0f0d23',
+        },
+      },
     },
   },
   plugins: [],
-}
+};
