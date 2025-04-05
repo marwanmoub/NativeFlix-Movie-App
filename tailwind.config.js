5ff562a8862b3db6e02d4714a55a8847c5ec67b7
marwanmoub <moubayedmarwancoding@gmai.com>
2025-04-05 09:57:12 -0700

    Implement tabs and movie screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#030014',
+        accent: '#AB8BFF',
+        secondary: "#151312",
+        light: {
+          100: "#D6C6FF",
+          200: "A8B5DB",
+          300: "9CA4AB"
+        },
+        dark: {
+          100: '#221F3D',
+          200: '#0f0d23'
+        }
+      }
+    },
   },
   plugins: [],
 }
-
